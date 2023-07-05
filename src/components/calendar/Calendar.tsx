fb663200d1c1a916e4301d1b8c63eddf2df8549a
nakagawa333 <naka33321@gmail.com>
2023-07-06 01:54:38 +0900

    カレンダー情報取得 不具合修正

--- src/components/calendar/Calendar.tsx
+++ src/components/calendar/Calendar.tsx
@@ -1,5 +1,5 @@
 import React, { Dispatch, SetStateAction, useLayoutEffect, useMemo, useState } from 'react';
-import { TableBody } from '@mui/material';
+import { Box, TableBody } from '@mui/material';
 import Table from '@mui/material/Table';
 import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
@@ -18,42 +18,29 @@ type Props = {
 }
 
 function Calendar(props:Props){
-
-    //現在時刻
-    const now:Dayjs = dayjs();
-    const startDay:Dayjs = now.startOf("month").subtract(now.startOf("month").day(),"d");
-    const endDay:Dayjs = now.endOf("month").add(6 - now.endOf("month").day() ,"d");
+    //日本の時刻に変換
+    dayjs.locale('ja');
     //祝日
     const [publicHoliday,setPublicHoliday] = useState<any>([]);
 
     const [calendarsEvent] = useCalendarsEvent(props,setPublicHoliday);
-    //日本の時刻に変換
-    dayjs.locale('ja');
 
-    let calendars:Dayjs[][] = [];
-    let plus:number = 0;
-    let isTrue:Boolean = true;
+    //カレンダー情報
+    const [calendars,setCalendars] = useState<Dayjs[][]>();
 
-    while(isTrue){
-        let day;
-        let arr:Dayjs[] = [];
-        for(let i = 0; i <= 6; i++){
-            day = startDay.add(plus,"d");
-            arr.push(day);
-            plus += 1;
-        }
-
-        calendars.push(arr);
-        if(day && day.month() === endDay.month() && day.day() === endDay.day()){
-            isTrue = false;
-        }
-    }
-
-    const options = {era: 'long'};
+    const [now,setNow] = useState<Dayjs>(dayjs());
+    //対象月初
+    const [targetBeginMonth,setTargetBeginMonth] = useState<Dayjs>(now.startOf("month"))
+    //開始時刻
+    const [startDay,setStartDay] = useState<Dayjs>(targetBeginMonth.subtract(targetBeginMonth.day(),"d"));
+    //終了時刻
+    const [endDay,setEndDay] = useState<Dayjs>(now.endOf("month").add(6 - now.endOf("month").day() ,"d"));
     //年号
-    let yearNum:string = now.toDate().toLocaleString( "ja-JP-u-ca-japanese" ,{ era : "short" }).split("/")[0] + "年";
+    const [yearNum,setYearNum] = useState<string>("");
 
     useLayoutEffect(() => {
+        //現在の時刻データを取得する
+        getNowDayInfo();
         Promise.all(([calendarsEvent.getSchedules(startDay,endDay),
             calendarsEvent.getPublicHoliday(now)]))
         .then((values:any) => {
@@ -64,49 +51,131 @@ function Calendar(props:Props){
         })
     },[])
 
+    /**
+     * 前月もしくは次月のデータ情報を取得する
+     * @param eventType イベントタイプ 1:前月,2:次月
+     */
+    const getPreAfterDayInfo = (eventType:number) => {
+        let thisTargetBeginMonth = dayjs(targetBeginMonth);
+        if(eventType === 1){
+            thisTargetBeginMonth = dayjs(thisTargetBeginMonth).subtract(1,"M");
+        } else if(eventType === 2){
+            thisTargetBeginMonth = dayjs(thisTargetBeginMonth).add(1,"M");
+        }
+        let thisStartDay = thisTargetBeginMonth.subtract(thisTargetBeginMonth.day(),"d");
+        let thisEndDay = thisTargetBeginMonth.endOf("month").add(6 - thisTargetBeginMonth.endOf("month").day() ,"d");
+
+        setTargetBeginMonth(thisTargetBeginMonth);
+        setStartDay(thisStartDay);
+        setEndDay(thisEndDay);
+
+        let thisCalendars = [];
+        let plus:number = 0;
+        let isTrue:Boolean = true;
+    
+        while(isTrue){
+            let day;
+            let arr:Dayjs[] = [];
+            for(let i = 0; i <= 6; i++){
+                day = thisStartDay.add(plus,"d");
+                arr.push(day);
+                plus += 1;
+            }
+    
+            thisCalendars.push(arr);
+            if(day && day.month() === thisEndDay.month() && day.day() === thisEndDay.day()
+               && day.date() === thisEndDay.date()){
+                isTrue = false;
+            }
+        }
+
+        //カレンダー情報更新
+        setCalendars(thisCalendars);
+        //年号更新
+        setYearNum(thisTargetBeginMonth.toDate().toLocaleString( "ja-JP-u-ca-japanese" ,{ era : "short" }).split("/")[0]);
+
+    }
+
+    /**
+     * 現在の時刻データを取得する
+     */
+    const getNowDayInfo = () => {
+        let thisCalendars = [];
+        let plus:number = 0;
+        let isTrue:Boolean = true;
+    
+        while(isTrue){
+            let day;
+            let arr:Dayjs[] = [];
+            for(let i = 0; i <= 6; i++){
+                day = startDay.add(plus,"d");
+                arr.push(day);
+                plus += 1;
+            }
+    
+            thisCalendars.push(arr);
+            if(day && day.month() === endDay.month() && day.day() === endDay.day()){
+                isTrue = false;
+            }
+        }
+
+        //カレンダー情報更新
+        setCalendars(thisCalendars);
+        //年号更新
+        setYearNum(now.toDate().toLocaleString( "ja-JP-u-ca-japanese" ,{ era : "short" }).split("/")[0]);
+    }
+
     return(
         <>
-        <div style={{display:"flex"}}>
-            <div>
-              <p>{yearNum}</p>
-            </div>
+        <Box>
+            <div style={{display:"flex"}}>
+                <div>
+                <p>{yearNum}</p>
+                </div>
 
-            <div style={{display:"flex",margin:"auto"}}>
-                <p>{startDay.year()}年</p>
-                <p style={{marginLeft:"10px"}}>{now.month() + 1}月</p>                
+                <p onClick={() => getPreAfterDayInfo(1)}>前</p>
+                <div style={{display:"flex",margin:"auto"}}>
+                    <p>{startDay.year()}年</p>
+                    <p style={{marginLeft:"10px"}}>{targetBeginMonth.month() + 1}月</p>                
+                </div>
+
+                <p onClick={() => getPreAfterDayInfo(2)}>次</p>
             </div>
-        </div>
-        <TableContainer style={{marginBottom:"30px"}}>
-            <Table>
-                <TableRow>
-                     <TableCell style={{color:"red"}}>日</TableCell>
-                     <TableCell>月</TableCell>
-                     <TableCell>火</TableCell>
-                     <TableCell>水</TableCell>
-                     <TableCell>木</TableCell>
-                     <TableCell>金</TableCell>
-                     <TableCell style={{color:"aqua"}}>土</TableCell>
-                </TableRow>
-                {
-                  calendars.map((arr:any,index:number) => {
-                        return(
-                            <TableBody key={index}>
-                              {
-                                arr.map((value:Dayjs,j:number) => {
-                                    return (
-                                        <TableCell 
-                                           style={{color:calendarsEvent.getColor(value,publicHoliday),background:calendarsEvent.getBackGroudColor(value,now)}} 
-                                           key={j}>{value.date()}
-                                        </TableCell>
-                                    )
-                                })
-                              }
-                            </TableBody>
-                        )
-                    })
-                }
-            </Table>
-        </TableContainer>
+        </Box>
+
+        <Box>
+            <TableContainer style={{marginBottom:"30px"}}>
+                <Table>
+                    <TableRow>
+                        <TableCell style={{color:"red"}}>日</TableCell>
+                        <TableCell>月</TableCell>
+                        <TableCell>火</TableCell>
+                        <TableCell>水</TableCell>
+                        <TableCell>木</TableCell>
+                        <TableCell>金</TableCell>
+                        <TableCell style={{color:"aqua"}}>土</TableCell>
+                    </TableRow>
+                    {
+                    calendars && calendars.map((arr:any,index:number) => {
+                            return(
+                                <TableBody key={index}>
+                                {
+                                    arr.map((value:Dayjs,j:number) => {
+                                        return (
+                                            <TableCell 
+                                            style={{color:calendarsEvent.getColor(value,publicHoliday),background:calendarsEvent.getBackGroudColor(value,now)}} 
+                                            key={j}>{value.date()}
+                                            </TableCell>
+                                        )
+                                    })
+                                }
+                                </TableBody>
+                            )
+                        })
+                    }
+                </Table>
+            </TableContainer>
+        </Box>
         </>
     )
 }
