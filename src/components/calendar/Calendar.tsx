74d284e5b0a6622f7c0b30aaa0fdafe73d10b8aa
nakagawa333 <naka33321@gmail.com>
2023-07-08 17:43:04 +0900

    メソッドをhooksに移動

--- src/components/calendar/Calendar.tsx
+++ src/components/calendar/Calendar.tsx
@@ -27,12 +27,10 @@ function Calendar(props:Props){
     //祝日
     const [publicHoliday,setPublicHoliday] = useState<any>([]);
 
-    const [calendarsEvent] = useCalendarsEvent(props,setPublicHoliday);
-
     //カレンダー情報
     const [calendars,setCalendars] = useState<Dayjs[][]>();
 
-    const [now,setNow] = useState<Dayjs>(dayjs());
+    const now:Dayjs= dayjs();
     //対象月初
     const [targetBeginMonth,setTargetBeginMonth] = useState<Dayjs>(now.startOf("month"))
     //開始時刻
@@ -42,92 +40,25 @@ function Calendar(props:Props){
     //年号
     const [yearNum,setYearNum] = useState<string>("");
 
+    const [calendarsEvent] = useCalendarsEvent(props,
+        setPublicHoliday,setCalendars,setTargetBeginMonth,
+        setStartDay,setEndDay,setYearNum);
+
+
     useLayoutEffect(() => {
         //現在の時刻データを取得する
-        getNowDayInfo();
+        //calendarsEvent.getNowDayInfo(startDay,endDay,now);
         Promise.all(([calendarsEvent.getSchedules(startDay,endDay),
-            calendarsEvent.getPublicHoliday(now.year())]))
+            calendarsEvent.getPublicHoliday(now.year()),
+            calendarsEvent.getNowDayInfo(startDay,endDay,now)
+        ]))
         .then((values:any) => {
-            console.info("処理に成功しました");
+             console.info("処理に成功しました");
         })
         .catch((error:any) => {
             console.error(error);
         })
     },[])
-
-    /**
-     * 前月もしくは次月のデータ情報を取得する
-     * @param eventType イベントタイプ 1:前月,2:次月
-     */
-    const getPreAfterDayInfo = (eventType:number) => {
-        let thisTargetBeginMonth = dayjs(targetBeginMonth);
-        if(eventType === 1){
-            thisTargetBeginMonth = dayjs(thisTargetBeginMonth).subtract(1,"M");
-        } else if(eventType === 2){
-            thisTargetBeginMonth = dayjs(thisTargetBeginMonth).add(1,"M");
-        }
-        let thisStartDay = thisTargetBeginMonth.subtract(thisTargetBeginMonth.day(),"d");
-        let thisEndDay = thisTargetBeginMonth.endOf("month").add(6 - thisTargetBeginMonth.endOf("month").day() ,"d");
-
-        setTargetBeginMonth(thisTargetBeginMonth);
-        setStartDay(thisStartDay);
-        setEndDay(thisEndDay);
-
-        let thisCalendars = [];
-        let plus:number = 0;
-        let isTrue:Boolean = true;
-    
-        while(isTrue){
-            let day;
-            let arr:Dayjs[] = [];
-            for(let i = 0; i <= 6; i++){
-                day = thisStartDay.add(plus,"d");
-                arr.push(day);
-                plus += 1;
-            }
-    
-            thisCalendars.push(arr);
-            if(day && day.month() === thisEndDay.month() && day.day() === thisEndDay.day()
-               && day.date() === thisEndDay.date()){
-                isTrue = false;
-            }
-        }
-
-        //カレンダー情報更新
-        setCalendars(thisCalendars);
-        //年号更新
-        setYearNum(thisTargetBeginMonth.toDate().toLocaleString( "ja-JP-u-ca-japanese" ,{ era : "short" }).split("/")[0]);
-
-    }
-
-    /**
-     * 現在の時刻データを取得する
-     */
-    const getNowDayInfo = () => {
-        let thisCalendars = [];
-        let plus:number = 0;
-        let isTrue:Boolean = true;
-    
-        while(isTrue){
-            let day;
-            let arr:Dayjs[] = [];
-            for(let i = 0; i <= 6; i++){
-                day = startDay.add(plus,"d");
-                arr.push(day);
-                plus += 1;
-            }
-    
-            thisCalendars.push(arr);
-            if(day && day.month() === endDay.month() && day.day() === endDay.day()){
-                isTrue = false;
-            }
-        }
-
-        //カレンダー情報更新
-        setCalendars(thisCalendars);
-        //年号更新
-        setYearNum(now.toDate().toLocaleString( "ja-JP-u-ca-japanese" ,{ era : "short" }).split("/")[0]);
-    }
 
     return(
         <>
@@ -140,7 +71,7 @@ function Calendar(props:Props){
                 <Box style={{display:"flex",margin:"auto"}}>
                     <Box style={{marginTop:"15px",marginRight:"20px"}}>
                     <ArrowBackIcon 
-                    onClick={() => getPreAfterDayInfo(1)}></ArrowBackIcon>
+                    onClick={() => calendarsEvent.getPreAfterDayInfo(targetBeginMonth,1)}></ArrowBackIcon>
                     </Box>
                     
                     <p>{startDay.year()}年</p>
@@ -148,7 +79,7 @@ function Calendar(props:Props){
 
                     <Box style={{marginTop:"15px",marginLeft:"20px"}}>
                         <ArrowForwardIcon 
-                        onClick={() => getPreAfterDayInfo(2)}>
+                        onClick={() => calendarsEvent.getPreAfterDayInfo(targetBeginMonth,2)}>
                         </ArrowForwardIcon>
                     </Box>  
                 </Box>
--- src/hooks/CalendarsEvent.ts
+++ src/hooks/CalendarsEvent.ts
@@ -7,11 +7,18 @@ type CalendarsEvent = {
     getPublicHoliday: (year:number) => void;
     getColor: (day:Dayjs,publicHoliday:any) => string;
     getBackGroudColor: (day:Dayjs,now:Dayjs) => string;
+    getNowDayInfo:(startDay:Dayjs,endDay:Dayjs,now:Dayjs) => void;
+    getPreAfterDayInfo:(targetBeginMonth:Dayjs,eventType:number) => void;
 }
 
 export const useCalendarsEvent = (
     props:any,
-    setPublicHoliday:Dispatch<SetStateAction<any[]>>
+    setPublicHoliday:Dispatch<SetStateAction<any[]>>,
+    setCalendars:Dispatch<SetStateAction<Dayjs[][] | undefined>>,
+    setTargetBeginMonth:Dispatch<SetStateAction<Dayjs>>,
+    setStartDay:Dispatch<SetStateAction<Dayjs>>,
+    setEndDay:Dispatch<SetStateAction<Dayjs>>,
+    setYearNum:Dispatch<SetStateAction<string>>
 ): [CalendarsEvent] => {
     /**
      * スケジュール情報を取得する
@@ -47,6 +54,83 @@ export const useCalendarsEvent = (
             console.error(error);
             console.error("Googleカレンダー情報の取得に失敗しました");
         }
+    }
+
+    /**
+     * 前月もしくは次月のデータ情報を取得する
+     * @param eventType イベントタイプ 1:前月,2:次月
+     */
+    const getPreAfterDayInfo = (targetBeginMonth:Dayjs,eventType:number) => {
+        let thisTargetBeginMonth = dayjs(targetBeginMonth);
+        if(eventType === 1){
+            thisTargetBeginMonth = dayjs(thisTargetBeginMonth).subtract(1,"M");
+        } else if(eventType === 2){
+            thisTargetBeginMonth = dayjs(thisTargetBeginMonth).add(1,"M");
+        }
+        let thisStartDay = thisTargetBeginMonth.subtract(thisTargetBeginMonth.day(),"d");
+        let thisEndDay = thisTargetBeginMonth.endOf("month").add(6 - thisTargetBeginMonth.endOf("month").day() ,"d");
+
+        setTargetBeginMonth(thisTargetBeginMonth);
+        setStartDay(thisStartDay);
+        setEndDay(thisEndDay);
+
+        let thisCalendars = [];
+        let plus:number = 0;
+        let isTrue:Boolean = true;
+    
+        while(isTrue){
+            let day;
+            let arr:Dayjs[] = [];
+            for(let i = 0; i <= 6; i++){
+                day = thisStartDay.add(plus,"d");
+                arr.push(day);
+                plus += 1;
+            }
+    
+            thisCalendars.push(arr);
+            if(day && day.month() === thisEndDay.month() && day.day() === thisEndDay.day()
+               && day.date() === thisEndDay.date()){
+                isTrue = false;
+            }
+        }
+
+        //カレンダー情報更新
+        setCalendars(thisCalendars);
+        //年号更新
+        setYearNum(thisTargetBeginMonth.toDate().toLocaleString( "ja-JP-u-ca-japanese" ,{ era : "short" }).split("/")[0]);
+
+    }
+
+    /**
+     * 現在の時刻データを取得する
+     * @param startDay 表示開示日時
+     * @param endDay 表示終了日時
+     * @param now 現在時刻
+     */
+    const getNowDayInfo = (startDay:Dayjs,endDay:Dayjs,now:Dayjs) => {
+        let thisCalendars = [];
+        let plus:number = 0;
+        let isTrue:Boolean = true;
+    
+        while(isTrue){
+            let day;
+            let arr:Dayjs[] = [];
+            for(let i = 0; i <= 6; i++){
+                day = startDay.add(plus,"d");
+                arr.push(day);
+                plus += 1;
+            }
+    
+            thisCalendars.push(arr);
+            if(day && day.month() === endDay.month() && day.day() === endDay.day()){
+                isTrue = false;
+            }
+        }
+
+        //カレンダー情報更新
+        setCalendars(thisCalendars);
+        //年号更新
+        setYearNum(now.toDate().toLocaleString( "ja-JP-u-ca-japanese" ,{ era : "short" }).split("/")[0]);        
     }
 
     /**
@@ -112,5 +196,5 @@ export const useCalendarsEvent = (
         return "white";
     }
 
-    return [{ getSchedules, getPublicHoliday,getColor,getBackGroudColor}];
+    return [{ getSchedules, getPublicHoliday,getColor,getBackGroudColor,getNowDayInfo,getPreAfterDayInfo}];
 };
